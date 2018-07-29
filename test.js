f57ac6855acf1bb79a33b5be46cbb6a25d832651
xiaobao@shuanger <xiaobaoshuanger@xiaobaoshuangerdeMacBook-Pro.local>
2018-07-29 09:35:24 +0800

    ES6新特新总结、三种排序算法实现、JS模式整理

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
-let a=[1,2,3,4,5]
-let b = a.filter((item)=> 0==item%2)
-console.log(b)
+let a=[2,4,5,6,1,0,3]
+function quickSort(arr){
+    let len=arr.length
+    if(len<=1){
+        return arr
+    }
+    let pre=[]
+    let tail=[]
+    let mid=arr[0]
+    for(let i=1;i<len;i++){
+        if(arr[i] < mid){
+            pre.push(arr[i])
+        }
+        if (arr[i] > mid){
+            tail.push(arr[i])
+        }
+        
+    }
+   return [ ...quickSort(pre), mid, ...quickSort(tail)]
+}
+console.log( quickSort(a))
